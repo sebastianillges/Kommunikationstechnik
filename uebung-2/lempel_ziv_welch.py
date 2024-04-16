84f31912e29a4f1b8e0ba1a036d13860d1e90f4f
Dominik Bartsch <dominik.bartsch@gmx.net>
2024-04-16 15:35:03 +0200

    unfertige Lösung Aufgabe 2

---
+++ uebung-2/lempel_ziv_welch.py
@@ -0,0 +1,42 @@
+# lempel_ziv_welch.py
+
+class LempelZivWelch:
+    @staticmethod
+    def encode(message, char_list):
+        # dictionary = {char: i for i, char in enumerate(char_list)}
+        bitstring = b''
+        pattern = ''
+
+        for char in message:
+            if pattern + char in char_list:
+                pattern = pattern + char
+            else:
+                
+
+            char_list.append(char)
+
+        return bitstring
+
+
+    @staticmethod
+    def decode(bitstring, char_list):
+        dictionary = {char: i for i, char in enumerate(char_list)}
+        message = ''
+
+        return message
+
+
+def main():
+    message = 'FISCHERSFRITZFISCHTFRISCHEFISCHE'
+    char_list = list(set(message))
+
+    bitstring = LempelZivWelch.encode(message, char_list.copy())
+    new_message = LempelZivWelch.decode(bitstring, char_list.copy())
+
+    print(f'original message : "{message}"')
+    print(f'encoded bitstring: {bitstring}')
+    print(f'decoded bitstring: "{new_message}"')
+
+
+if __name__ == '__main__':
+    main()
