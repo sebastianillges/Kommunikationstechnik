fd840450b12f994cdc1b9547ae0bd4b5331538ff
Dominik Bartsch <dominik.bartsch@gmx.net>
2024-05-11 21:16:17 +0200

    small refactorings

--- uebung-3/block_codes.py
+++ uebung-3/block_codes.py
@@ -16,16 +16,6 @@ class BlockCodes:
                                             axis=1).astype(int)
         self.parity_matrix_transposed = np.transpose(self.parity_matrix)
         self.syndrome_table = self._build_syndrome_table()
-        print("##############################")
-        for i, x in self.syndrome_table.items():
-            print(i, x)
-
-    def build_syndrome_table(self):
-        syndrome_table = [[0] * len(self.parity_matrix_transposed) for _ in range(len(self.parity_matrix_transposed) + 1)]
-        for i, column in enumerate(self.parity_matrix_transposed):
-            index = int(''.join([f'{b}' for b in column]), 2)
-            syndrome_table[index][i] = 1
-        return np.array(syndrome_table)
 
     def _build_syndrome_table(self):
         zero_dict = {0: np.zeros(self.code_bits).astype(int)}
@@ -41,10 +31,6 @@ class BlockCodes:
                 e = np.identity(len(decimal_values1)).astype(int)
                 error_dict1 = dict(zip(decimal_values1, e))
 
-                # par2 = [(i + j) % 2 for i in self.parity_matrix_transposed for j in self.parity_matrix_transposed]
-                # decimal_values2 = [int(''.join(map(str, row)), 2) for row in par2]
-                # error2 = [(i + j) % 2 for i in np.identity(len(decimal_values2)).astype(int) for j in np.identity(len(decimal_values2)).astype(int)]
-                # error_dict2 = dict(zip(decimal_values2, error2))
                 par2 = np.array([(i + j) % 2 for i in self.parity_matrix_transposed for j in self.parity_matrix_transposed])
                 decimal_values2 = np.array([int(''.join(map(str, row)), 2) for row in par2])
                 error2 = [(i + j) % 2 for i in np.identity(self.code_bits).astype(int) for j in np.identity(self.code_bits).astype(int)]
@@ -61,15 +47,13 @@ class BlockCodes:
     def decode(self, codeword):
         check_type_np_int_array(codeword)
         error_syndrome = codeword @ self.parity_matrix_transposed % 2
-        print(f'error syndrom: {error_syndrome}')
         error_syndrome_num = int(''.join(map(str, error_syndrome)), 2)
         error_vector = self.syndrome_table.get(error_syndrome_num)
-        print(f'error_syndrome_num: {error_syndrome_num}')
-        print(f'error_vector: {error_vector}')
         if error_vector is None:
             return None
-        message = (codeword ^ error_vector)[self.check_bits:]
-        return message, sum(error_vector)
+        else:
+            message = (codeword ^ error_vector)[self.check_bits:]
+            return message, sum(error_vector)
 
     def __str__(self):
         return f"""Block Codes Information:
@@ -97,28 +81,32 @@ def flip_n_bits(arr, n):
 
 def test(message, part_matrix, max_corr_bits):
     bcodes = BlockCodes(part_matrix, max_corr_bits)
-    print(bcodes, end='\n\n')
+    # print(bcodes, end='\n\n')
+    # for k, v in bcodes.syndrome_table.items():
+    #     print(f'{k}: {v}')
 
-    print(f'message: {message}')
+    print(f'          message: {message}')
     codeword = bcodes.encode(message)
-    print(f"encoded codeword: {codeword}")
+    print(f" encoded codeword: {codeword}")
 
     faulty_codeword = flip_n_bits(codeword, 2)
-    # faulty_codeword = np.array([1, 0, 1, 0, 1, 1, 1, 1])
-    print(f"faulty codeword: {faulty_codeword}")
+    print(f"  faulty codeword: {faulty_codeword}")
 
-    if recv:= bcodes.decode(faulty_codeword):
+    if recv := bcodes.decode(faulty_codeword):
         new_message, corrected_errors = recv
         print(f'received messsage: {new_message}')
-        print(f'number of corrected errors: {corrected_errors}')
+        print(f' corrected errors: {corrected_errors}')
     else:
         print('Could not correct errors')
 
 
 def main():
-    # test(np.array([0, 1, 1, 0]), np.array([[1, 1, 0], [0, 1, 1], [1, 1, 1], [1, 0, 1]]), 1)  # Test 1
-    # test(np.array([1, 1]), np.array([[1, 1, 1, 1, 0, 0], [0, 0, 1, 1, 1, 1]]), 2)  # Test 2
-    test(np.array([1, 1, 0]), np.array([[1, 1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 1, 0], [1, 0, 1, 0, 1, 0, 1]]), 2)  # Test 3
+    print('Test 1:')
+    test(np.array([0, 1, 1, 0]), np.array([[1, 1, 0], [0, 1, 1], [1, 1, 1], [1, 0, 1]]), 1)
+    print('\nTest 2:')
+    test(np.array([1, 1]), np.array([[1, 1, 1, 1, 0, 0], [0, 0, 1, 1, 1, 1]]), 2)
+    print('\nTest 3:')
+    test(np.array([1, 1, 0]), np.array([[1, 1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 1, 0], [1, 0, 1, 0, 1, 0, 1]]), 2)
 
 
 if __name__ == '__main__':
